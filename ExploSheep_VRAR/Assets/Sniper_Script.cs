5304106b247dec08a3d227f30f8d1a165c34ac73
Agralerith <49476738+Agralerith@users.noreply.github.com>
2020-12-19 13:40:12 +0800

    FPS

---
+++ ExploSheep_VRAR/Assets/Sniper_Script.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sniper_Script : MonoBehaviour
+{
+    public float damage = 10f;
+
+    public float range = 100f;
+
+    public Camera fpsCam;
+
+    Sheep sheep;
+
+    RaycastHit hits;
+
+    public static bool isInfectedSheep;
+    public static bool isHealthySheep;
+
+    public AudioSource sniperFire;
+
+    public ParticleSystem sniperMuzzleFlash;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isInfectedSheep = false;
+        isHealthySheep = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hits, range))
+        {
+            //Debug.Log(hits.transform.name);
+
+            sheep = hits.transform.GetComponent<Sheep>();
+
+            //if (sheep != null)
+            //{
+            //    Debug.Log("Starting Coroutine");
+            //    StartCoroutine(TwoSecondsCoroutine());               
+            //}
+
+            if (hits.collider.tag == "InfectedSheep")
+            {
+                Debug.Log("isInfectedSheep");
+                isInfectedSheep = true;
+                isHealthySheep = false;
+                StartCoroutine(TwoSecondsCoroutine());       
+            }
+            else
+            if (hits.collider.tag == "Sheep")
+            {
+                //Debug.Log("isSheep");
+                isHealthySheep = true;
+                isInfectedSheep = false;
+                StartCoroutine(TwoSecondsCoroutine());
+            }
+            else
+            {
+                //Debug.Log("not Sheep");
+                isInfectedSheep = false;
+                isHealthySheep = false;
+                StopCoroutine(TwoSecondsCoroutine());
+            }
+        }
+        else
+        {
+            //Debug.Log("not Sheep");
+            isInfectedSheep = false;
+            isHealthySheep = false;
+            StopCoroutine(TwoSecondsCoroutine());
+        }
+
+    }
+
+    IEnumerator TwoSecondsCoroutine()
+    {
+        yield return new WaitForSeconds(2);
+        if (isInfectedSheep && !isHealthySheep && CameraRaycast.loader.fillAmount == 1)
+        {
+            Debug.Log("Infected Sheep :(");
+            sniperFire.Play();
+            sniperMuzzleFlash.Play();
+            sheep.TakeDamge(damage);
+        }
+        
+        if (isHealthySheep && !isInfectedSheep && CameraRaycast.loader.fillAmount == 1)
+        {
+            Debug.Log("Healthy Sheep :)");
+            sniperFire.Play();
+            sniperMuzzleFlash.Play();
+            sheep.TakeDamge(damage);
+        }
+    }
+}
--- ExploSheep_VRAR/Assets/script/CameraRaycast.cs
+++ ExploSheep_VRAR/Assets/script/CameraRaycast.cs
@@ -8,10 +8,13 @@ public class CameraRaycast : MonoBehaviour
     Camera cam;
     public Image loadingCircle;
 
+    public static Image loader;
+
     // Start is called before the first frame update
     void Start()
     {
         cam = GetComponent<Camera>();
+        loader = loadingCircle;
     }
 
     // Update is called once per frame
@@ -21,14 +24,12 @@ public class CameraRaycast : MonoBehaviour
         RaycastHit hit;
         if (Physics.Raycast(ray, out hit))
         {
-            if(hit.transform.tag == "Teleport" || hit.transform.tag == "WeirdMenu")
+            if(hit.transform.tag == "Teleport" || hit.transform.tag == "WeirdMenu" || hit.transform.tag == "InfectedSheep" || hit.transform.tag == "Sheep")
             {
-                loadingCircle.fillAmount += Time.deltaTime / 1f;
-                if(loadingCircle.fillAmount >= 1)
-                {
-                    loadingCircle.fillAmount = 0;
-                }
+                loadingCircle.fillAmount += Time.deltaTime / 2f;
             }
+            else
+                loadingCircle.fillAmount = 0;
         }
         else
         {
