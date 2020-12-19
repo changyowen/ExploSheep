bb27f3bd725a32e76054568a26926f777aab7176
benokelvin <kelvinlee981@outlook.com>
2020-12-19 21:28:45 +0800

    HP System

---
+++ ExploSheep_VRAR/Assets/script/HPScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HPScript : MonoBehaviour
+{
+    public int HP = 3;
+    public GameObject[] HPImage = new GameObject[3];
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < 3; i++)
+        {
+            HPImage[i].gameObject.SetActive(true);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void DeductHP()
+    {
+        HP--;
+        if (HP < 3)
+        {
+            for(int i = HP; i < 4; i++)
+            {
+                HPImage[i].SetActive(false);
+            }
+        }
+    }
+}
