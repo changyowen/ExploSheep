d927befa4e55f5f640b5fe8a26e365c4a5833f93
benokelvin <kelvinlee981@outlook.com>
2020-12-20 13:38:13 +0800

    ScoreHandler and ReturnScript

--- ExploSheep_VRAR/Assets/script/ScoreHandler.cs
+++ ExploSheep_VRAR/Assets/script/ScoreHandler.cs
@@ -5,13 +5,29 @@ using UnityEngine.UI;
 
 public class ScoreHandler : MonoBehaviour
 {
+    public int HighScore;
     public Text ScoreText;
+    public Text GameOverScore;
     public int score = 0;
+    public Text HighScoreText;
 
     // Update is called once per frame
+    void Start()
+    {
+        HighScore = PlayerPrefs.GetInt("HighScore");
+    }
     void Update()
     {
         ScoreText.text = "Score : " + score;
+        GameOverScore.text = "Score :" + score;
+        if (score > HighScore)
+        {
+            HighScore = score;
+
+        }
+
+        HighScoreText.text = "High score : " + HighScore;
+        PlayerPrefs.SetInt("HighScore", HighScore);
     }
 
     public void AddScore()
