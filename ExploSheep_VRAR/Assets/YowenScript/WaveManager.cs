521aad1dbbd15d4a7b1a699f3048e38f3ddbad04
changyowen <changyowen@gmail.com>
2020-12-23 03:44:37 +0800

    small update

--- ExploSheep_VRAR/Assets/YowenScript/WaveManager.cs
+++ ExploSheep_VRAR/Assets/YowenScript/WaveManager.cs
@@ -60,7 +60,6 @@ public class WaveManager : MonoBehaviour
                 {
                     waveCount++;
                     startGame = false;
-                    //StartWave();
                 }
             }
         }
